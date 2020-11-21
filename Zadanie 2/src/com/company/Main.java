ab479b38cb4ab60e513bd2e167274b74ed05ba0e
RAWESOUL <ROLANDOS-IRK@YANDEX.RU>
2020-11-21 18:38:38 +0300

    Cкорректированы комментарии в программах

--- Zadanie 2/src/com/company/Main.java
+++ Zadanie 2/src/com/company/Main.java
@@ -5,23 +5,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //1. Выбираем тип операции
+        /**
+         *  1. Выбираем тип операции
+         */
         System.out.println("Введите тип арифметической операции\n(сложение, вычитание, умножение, деление)");
         Scanner in = new Scanner(System.in);
         String Type = in.nextLine();
-        //2. Вводим аргументы
-        System.out.println("Введите первое число (аргумент операции)");
+        /**
+         * 2. Вводим аргументы
+         */
+        System.out.println("Введите первое число");
         double pervoe = in.nextDouble();
-        System.out.println("Введите второе число (аргумент операции)");
+        System.out.println("Введите второе число");
         double vtoroe = in.nextDouble();
-        //2.1. Проверка отсутствия деления на ноль
+        /**
+         * 2.1. Проверка отсутствия деления на ноль
+         */
         if ((Type.equals("деление")) & (vtoroe == 0)) {
             while (vtoroe==0) {
                 System.out.println("Деление на ноль. Введите другое число");
                 vtoroe = in.nextDouble();
             }
         };
-        //3. Вычисляем результат и выводим округленное значение
+        /**
+         * 3. Вычисляем результат и выводим округленное значение
+         */
         double result = switch (Type) {
             case "сложение" -> pervoe + vtoroe;
             case "вычитание" -> pervoe - vtoroe;
--- Zadanie 3/src/com/company/Main.java
+++ Zadanie 3/src/com/company/Main.java
@@ -10,23 +10,31 @@ public class Main {
         byte proga = in.nextByte();
         switch (proga) {
             case 1 -> {
-                //1. Выбираем тип операции
+                /**
+                 *  1. Выбираем тип операции
+                 */
                 System.out.println("Введите тип арифметической операции\n(сложение, вычитание, умножение, деление)");
                 Scanner calc = new Scanner(System.in);
                 String Type = calc.nextLine();
-                //2. Вводим аргументы
+                /**
+                 * 2. Вводим аргументы
+                 */
                 System.out.println("Введите первое число (аргумент операции)");
                 double pervoe = in.nextDouble();
                 System.out.println("Введите второе число (аргумент операции)");
                 double vtoroe = in.nextDouble();
-                //2.1. Проверка отсутствия деления на ноль
+                /**
+                 *  2.1. Проверка отсутствия деления на ноль
+                 */
                 if ((Type.equals("деление")) & (vtoroe == 0)) {
                     while (vtoroe == 0) {
                         System.out.println("Деление на ноль. Введите другое число");
                         vtoroe = in.nextDouble();
                     }
                 }
-                //3. Вычисляем результат и выводим округленное значение
+                /**
+                 * 3. Вычисляем результат и выводим округленное значение
+                 */
                 double result = switch (Type) {
                     case "сложение" -> pervoe + vtoroe;
                     case "вычитание" -> pervoe - vtoroe;
@@ -38,14 +46,20 @@ public class Main {
                 System.out.printf("%.4f", result);
                 }
             case 2 -> {
-               //Определяем длину массива
+                /**
+                 * 1. Определяем длину массива
+                 */
                 Scanner vvod = new Scanner(System.in);
                 System.out.println("Введите количество слов");
                 int Num = vvod.nextInt();
-                //Инициализируем массив
+                /**
+                 * 2.Инициализируем массив и переводим чтение с консоли на следующую строку
+                 */
                 String[] arr = new String[Num];
                 arr[0] = vvod.nextLine();
-                // Вводим сравниваем значения
+                /**
+                 * 3. Вводим и сравниваем значения
+                 */
                 int max = 0, Nmax = 0;
                 for (int i = 1; i<Num+1; i++) {
                     System.out.println("Введите значение элемента массива №=" + i);
